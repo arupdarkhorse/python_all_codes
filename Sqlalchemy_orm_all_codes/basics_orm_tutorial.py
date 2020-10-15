3bc7c7c7977505b36229787a9e89c903b44c7b1a
arupdarkhorse <arupananda1996@gmail.com>
2020-10-16 02:09:17 +0530

    filtering added

--- Sqlalchemy_orm_all_codes/basics_orm_tutorial.py
+++ Sqlalchemy_orm_all_codes/basics_orm_tutorial.py
@@ -17,5 +17,15 @@ ins = [{"id" : i, "name" : f"arup_{str(i)}"} for i in range(100000)]
 conn.execute(arup_table.insert(), ins)
 
 # selcting rows from table
-temp = conn.execute(select[arup_table])
-print(temp.fetchmany(10))
+    #temp = conn.execute(select[arup_table])
+    #print(temp.fetchmany(10))
+
+cnt = 0
+while True:
+    df = pd.DataFrame(conn.executemany(10000))
+    if len(df)==0:
+        cnt +=1
+        break
+    else:
+        filename = f"arup_{str(cnt)}"
+        df.to_csv(filename, index=False)
